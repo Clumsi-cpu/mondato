2131d0319a1acc1a4f990fde861ce1f990831518
Clumsi-cpu <maximojoelr@gmail.com>
2024-11-21 20:24:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+public class TablasMultiplicarInfinitas
+{
+    public static void Main(string[] args)
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = x; i <= y; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine("{0}x{1}= {2}", i, j, i * j);
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
